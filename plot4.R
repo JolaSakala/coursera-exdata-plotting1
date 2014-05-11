94b1b259544cdf782bd82dc33e5959fa719a8929
guindilla <guillermo.fernandez.castellanos@gmail.com>
2014-05-11 17:43:15 -0500

    Fourth exercise finished

---
+++ plot4.R
@@ -0,0 +1,79 @@
+file.name <- "./household_power_consumption.txt"
+url       <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+zip.file  <- "./data.zip"
+
+# Check if the data is downloaded and download when applicable
+if (!file.exists("./household_power_consumption.txt")) {
+    download.file(url, destfile = zip.file, method = "curl")
+    unzip(zip.file)
+    file.remove(zip.file)
+}
+
+# Reading the file
+library(data.table)
+DT <- fread(file.name,
+            sep = ";",
+            header = TRUE,
+            colClasses = rep("character",9))
+
+# Convert "?" in NAs
+DT[DT == "?"] <- NA
+
+# Selecting adequate lines
+DT$Date <- as.Date(DT$Date, format = "%d/%m/%Y")
+DT <- DT[DT$Date >= as.Date("2007-02-01") & DT$Date <= as.Date("2007-02-02"),]
+
+# Joining day and time to create a new posix date
+mydates <- strptime(paste(DT$Date, DT$Time, sep = " "), format = "%Y-%m-%d %H:%M:%S")
+myseconds <- as.numeric(mydates)
+DT$posix <- mydates
+
+# Convert column that we will use to correct class
+DT$Global_active_power <- as.numeric(DT$Global_active_power)
+
+# Do the graph
+png(file = "plot4.png", width = 480, height = 480, units = "px")
+
+## Setting 4 graphs
+par(mfrow = c(2, 2))
+
+## Graph 1
+plot(mydates,
+     DT$Global_active_power,
+     type = "l",
+     xlab = "",
+     ylab = "Global Active Power")
+
+## Graph 2
+plot(mydates,
+     DT$Voltage,
+     type = "l",
+     xlab = "datetime",
+     ylab = "Voltage")
+
+## Graph 3
+plot(mydates,
+     DT$Sub_metering_1,
+     type = "l",
+     xlab = "",
+     ylab = "Energy sub metering")
+points(mydates,
+       type = "l",
+       DT$Sub_metering_2,
+       col = "red")
+points(mydates,
+       type = "l",
+       DT$Sub_metering_3,
+       col = "blue")
+legend("topright", col = c("black", "blue", "red"),
+       legend = c("Sub_metering_1","Sub_metering_2", "Sub_metering_3"), lty = 1)
+
+## Graph 4
+plot(mydates,
+     DT$Global_reactive_power,
+     type = "l",
+     xlab = "datetime",
+     ylab = "Global_reactive_power")
+
+
+dev.off()  # Close the png file device
